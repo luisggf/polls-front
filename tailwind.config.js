845cb9973315eb383c376f34146ea731de130f9f
luisggf <luis.guigfonseca@gmail.com>
2024-07-20 00:13:17 -0300

    would you rather and ig main component built

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        custom: ['"Comic Neue"', "cursive"],
+      },
+      backgroundImage: {
+        "custom-hot-gradient":
+          "linear-gradient(220deg, #FFE53B -40%, #FF2525 60%)",
+        "custom-cold-gradient":
+          "linear-gradient(330deg, #0093ff 40%, #80D0DA 100%)",
+      },
+    },
+  },
+  plugins: [],
+};
